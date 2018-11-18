aece8e9fc8063ad576f01cbb75ddefd538130134
Ryan Norman <rnorman@covermymeds.com>
2018-11-18 14:28:30 -0500

    Initial commit!

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,16 @@
+if ENV['S3_BUCKET'] && ENV['AWS_ACCESS_KEY_ID'] && ENV['AWS_SECRET_ACCESS_KEY']
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region:                'us-east-2'
+    }
+    config.cache_dir = "#{Rails.root}/tmp/uploads"
+    config.fog_directory  = ENV['S3_BUCKET']
+    config.fog_public     = false
+    config.fog_authenticated_url_expiration = 1.week
+    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
+  end
+end
